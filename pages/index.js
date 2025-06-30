2e722db0b82243c40498103737dcfd7448b3d51c
junoh1999 <157660909+junoh1999@users.noreply.github.com>
2025-06-30 15:06:21 -0700

    Revert "Update index.js"
    This reverts commit 671c7a404949c3dd6b26032752337bfba0f7dfbf.

--- pages/index.js
+++ pages/index.js
@@ -281,7 +281,7 @@ const shouldExpand = isHovered || isExpanded;
       <div style={{
   display: "flex",
   justifyContent: "flex-end",
-  paddingRight: "12px",
+  paddingRight: "22px",
   marginBottom: "3px",
   height: "16px", // Fixed height to prevent layout shift
   alignItems: "baseline"
@@ -335,9 +335,8 @@ const shouldExpand = isHovered || isExpanded;
     justifyContent: "space-between",
     minHeight: "80px",
     width: shouldExpand ? "260px" : "180px",
- position: "absolute",  // ← CHANGE from "relative"
-    right: shouldExpand ? "0px" : "80px",  // ← REPLACE marginLeft
-    transition: "width 300ms ease, right 300ms ease",  // ← UPDATE transition
+    marginLeft: shouldExpand ? "0px" : "80px", // Add this - pushes smaller version to the right
+    transition: "width 300ms ease, margin-left 300ms ease", // Animate both
     position: "relative",
     boxSizing: "border-box", // ADD THIS LINE
   }}
